620b64edb77c5fc036285265038765a7a85375c9
zyuzzhang <360766414@qq.com>
2016-02-25 09:35:06 +0800

    更新行为日志记录模块

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=d_easyhin_his',
+    'dsn' => 'mysql:host=localhost;dbname=gzhoss',
     'username' => 'root',
     'password' => '123456',
     'charset' => 'utf8',
